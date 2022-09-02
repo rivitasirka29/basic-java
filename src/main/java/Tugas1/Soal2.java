8227cafe45a20435df6fd9fc53ea1b6dd51c0c81
Rivita <Rivita@sirka.io>
2022-09-02 16:33:17 +0700

    Menyelesaikan soal no.2 java

--- src/main/java/Tugas1/Soal2.java
+++ src/main/java/Tugas1/Soal2.java
@@ -7,7 +7,16 @@ public class Soal2 {
         int ganjil2 = 3;
         int genap2  = 4;
 
+        boolean even;
+        boolean odd;
+
+        even = true;
+        odd = false;
+
         //print ganjil genap
-        System.out.println("angka " + ganjil1 + " adalah bilangan ganjil");
+        System.out.println("angka " + ganjil1 + " adalah bilangan ganjil = " + even);
+        System.out.println("angka " + genap1 + " adalah bilangan genap = " + odd);
+        System.out.println("angka " + ganjil2 + " adalah bilangan ganjil = " + even);
+        System.out.println("angka " + genap2 + " adalah bilangan genap = " + odd);
     }
 }
